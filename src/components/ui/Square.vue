99bdd4ca747fcf0f0261191f76d0bb860325e2e0
William.Chuang <william.chuang@WilliamChuangs-MacBook-Pro.local>
2025-07-22 18:32:24 +0800

    Added - [function]  regular function done

---
+++ src/components/ui/Square.vue
@@ -0,0 +1,119 @@
+<template>
+    <div class="square" ref="squareRef">
+        <div class="square-dom" :style="{ 'offset-path': `path('${pathTopLeft}')` }" />
+        <div class="square-dom" :style="{ 'offset-path': `path('${pathBottomRight}')` }" />
+        <div class="square-box" />
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { ref, reactive, computed, watch, watchEffect, onMounted } from 'vue';
+
+    const squareRef = ref();
+    const pathTopLeft = ref('');
+    const pathBottomRight = ref('');
+
+    // 因 offset-path 不接受斷行，需去除空格& '\n'
+    const buildPath = (path: string) => path.replace(/\s+/g, ' ').trim();
+
+    const updatePaths = () => {
+        const rect = squareRef.value?.getBoundingClientRect();
+        if (!rect) return;
+
+        const w = rect.width;
+        const h = rect.height;
+        const r = 10; // 圓角半徑
+
+        // 左下角起始點
+        pathTopLeft.value = buildPath(
+            `M${r},2 
+            H${w - r} 
+            A${r},${r},0,0,1,${w - 2},${r} 
+            V${h - r} 
+            A${r},${r},0,0,1,${w - r},${h - 2} 
+            H${r} 
+            A${r},${r},0,0,1,2,${h - r} 
+            V${r} A${r},${r},0,0,1,${r},2`
+        );
+
+        // 右下角起始點
+        pathBottomRight.value = buildPath(`
+            M${w - r},${h - 2} 
+            H${r} 
+            A${r},${r},0,0,1,2,${h - r} 
+            V${r} 
+            A${r},${r},0,0,1,${r},2 
+            H${w - r} 
+            A${r},${r},0,0,1,${w - 2},${r} 
+            V${h - r} 
+            A${r},${r},0,0,1,${w - r},${h - 2}
+        `);
+    };
+
+    onMounted(() => {
+        updatePaths();
+
+        window.addEventListener('resize', updatePaths);
+
+        const observer = new ResizeObserver(updatePaths);
+        if (squareRef.value) observer.observe(squareRef.value);
+    });
+</script>
+
+<style lang="scss" scoped>
+    .square {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        border-radius: 7px;
+        overflow: hidden;
+
+        .square-box {
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: 2;
+            width: 100%;
+            height: 100%;
+            border-radius: 8px;
+            border: 4px solid #777;
+            overflow: hidden;
+        }
+
+        .square-dom {
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: 3;
+            width: 20px;
+            height: 4px;
+            background-color: white;
+            border-radius: 4px;
+
+            offset-rotate: auto;
+            offset-distance: 0%;
+            animation: move 2s ease infinite;
+        }
+
+        // .animation-top-left {
+        //     offset-path: path(
+        //         'M10,2  H110  A10,10,0,0,1,118,10  V110  A10,10,0,0,1,110,118  H10  A10,10,0,0,1,2,110  V10  A10,10,0,0,1,10,0'
+        //     );
+        // }
+
+        // .animation-bottom-right {
+        //     offset-path: path(
+        //         'M110,118  H10  A10,10,0,0,1,2,110  V10  A10,10,0,0,1,10,2  H110  A10,10,0,0,1,118,10  V110  A10,10,0,0,1,110,118'
+        //     );
+        // }
+    }
+
+    @keyframes move {
+        50% {
+            offset-distance: 50%;
+        } // 右下 停頓
+        100% {
+            offset-distance: 100%;
+        } // 左上 停頓
+    }
+</style>
--- src/views/basic_a1/index.vue
+++ src/views/basic_a1/index.vue
@@ -1,9 +1,31 @@
 <template>
-    <div class="basic_a1">
-        <div class="square">
-            <div class="square-dom animation-top-left" />
-            <div class="square-dom animation-bottom-right" />
-            <div class="square-box" />
+    <div class="basic-a1">
+        <div class="square-view" :style="{ gridTemplateColumns: `repeat(${squareCols}, 1fr)` }">
+            <Square v-for="(num, index) in squareCount" :key="index" />
+        </div>
+
+        <br />
+
+        <!-- 數量按鈕 -->
+        <div class="button-items">
+            <button
+                v-for="(btn, index) in btnList"
+                :key="index"
+                :class="{ active: selected === btn }"
+                @click="changeView(btn)"
+            >
+                {{ btn }}
+            </button>
+        </div>
+        <br />
+
+        <!-- 亂數開關 -->
+        <div class="radio-items">
+            <input type="radio" id="all" name="contact" value="email" checked />
+            <label for="all">all</label>
+
+            <input type="radio" id="random" name="contact" value="email" />
+            <label for="random">random</label>
         </div>
     </div>
 </template>
@@ -11,64 +33,64 @@
 <script setup lang="ts">
     import { ref, reactive, computed, watch, watchEffect, onMounted } from 'vue';
 
-    onMounted(() => {});
+    import Square from '@/components/ui/Square.vue';
+
+    const btnList = reactive<string[]>(['1x1', '3x3', '5x5', '10x10']);
+    const selected = ref('1x1');
+
+    const changeView = (btn: string) => {
+        selected.value = btn;
+    };
+
+    // 行排
+    const squareCols = computed(() => {
+        const [cols] = selected.value.split('x').map(Number);
+        return cols;
+    });
+
+    // 總數計算
+    const squareCount = computed(() => {
+        let [cols, rows] = selected.value.split('x').map(Number);
+        console.log(cols, rows);
+        return cols * rows;
+    });
 </script>
 
 <style lang="scss" scoped>
-    .basic_a1 {
+    .basic-a1 {
         width: 100vw;
         height: 100vh;
         padding: 1rem;
 
-        .square {
-            position: relative;
-            width: 120px;
-            height: 120px;
-            border-radius: 7px;
-            overflow: hidden;
+        .square-view {
+            width: 90vw;
+            height: 90vw;
+            margin: 0 auto;
 
-            .square-box {
-                position: absolute;
-                top: 0;
-                left: 0;
-                z-index: 2;
-                width: 100%;
-                height: 100%;
-                border-radius: 8px;
-                border: 4px solid #777;
-                overflow: hidden;
-            }
-
-            .square-dom {
-                position: absolute;
-                top: 0;
-                left: 0;
-                z-index: 3;
-                width: 30px;
-                height: 5px;
-                background-color: white;
-                border-radius: 2px;
-
-                offset-rotate: auto;
-                offset-distance: 0%;
-                animation: move 1s ease infinite;
-            }
-            .animation-top-left {
-                offset-path: path(
-                    'M10,2  H110  A10,10,0,0,1,118,10  V110  A10,10,0,0,1,110,118  H10  A10,10,0,0,1,2,110  V10  A10,10,0,0,1,10,0'
-                );
-            }
-
-            .animation-bottom-right {
-                offset-path: path(
-                    'M110,118  H10  A10,10,0,0,1,2,110  V10  A10,10,0,0,1,10,2  H110  A10,10,0,0,1,118,10  V110  A10,10,0,0,1,110,118'
-                );
-            }
+            display: grid;
+            gap: 0.2rem;
+            // grid-template-columns: repeat(1, 1fr);
+        }
+        .radio-items {
+            display: flex;
+        }
+        .active {
+            background-color: red;
         }
     }
-    @keyframes move {
-        to {
-            offset-distance: 100%;
+
+    button {
+        cursor: pointer;
+        margin: 0 0.5rem;
+        padding: 0 0.5rem;
+        border-radius: 1rem;
+        transition: 0.3s;
+        &:hover {
+            background-color: aquamarine;
         }
+    }
+
+    input {
+        margin: 0 0.5rem;
     }
 </style>
